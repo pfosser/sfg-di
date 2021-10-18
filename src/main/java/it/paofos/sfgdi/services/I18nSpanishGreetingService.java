b4fc9bbd06733f49d5e76c1d28873a32d1b6d644
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2021-10-18 13:17:42 +0200

    Default profile

--- src/main/java/it/paofos/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/it/paofos/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package it.paofos.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({ "ES", "default" })
 @Service("I18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
